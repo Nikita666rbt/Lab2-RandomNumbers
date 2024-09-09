e5eb2e987eda62046157d1e4e80e4620e8c632a6
Никита Литовка <nikituslitus@yandex.ru>
2024-09-09 15:56:39 +0300

    Написан генератор чисел

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hello World")
+import random
+
+
+def Generator(n):
+    return [random.randint(0, n) for i in range(n)]
+
+n = int(input("Введите размер списка: "))
+res = Generator(n)
+print("Сгенерированный список: ", res)
